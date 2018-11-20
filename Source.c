1536619240339027f303d4d9fa3264ce07ca0aa7
Admin <kaganyaknazar@gmail.com>
2018-11-20 13:29:37 +0200

    nothing was changed

--- Source.c
+++ Source.c
@@ -4,22 +4,23 @@
 #include <math.h>
 
 main() {
-	double a, b, h, z, y = 0, s = 0, x, k=0;
-	scanf_s("%lf", &a);
+	double maxVariableValue, b, cycleStep, z, mathFunction = 0, endSum = 0, x, k = 0;
+	scanf_s("%lf", &maxVariableValue);
 	scanf_s("%lf", &b);
 	scanf_s("%lf", &h);
 	scanf_s("%lf", &z); 
 
 
-	for (x = a; x <= b+h; x += h) {
+	for (x = maxVariableValue; x <= b+cycleStep; x += cycleStep) {
 		k = 1;
-		do { 
-			y = (pow(-1, k)*pow(x, 2 * k + 3)) / ((2 * k + 1)*(2 * k + 3)); 
-			s += y;
+		do {
+			mathFunction = (pow(-1, k)*pow(x, 2 * k + 3)) / ((2 * k + 1)*(2 * k + 3));
+			endSum += mathFunction;
 		}
-		while (y > z);
+		while (mathFunction > z);
 		printf("\n");
-		printf("x=%lf s=%lf", x, s);
+		printf("Змінна x = %lf ", x);
+		printf("Кінцева сума рівна f = %lf", endSum);
 
 	}
 	_getch();
